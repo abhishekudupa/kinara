4f2a30cd71ebc5bc7c071d44679b7d7c68ba1f3f
Mukund Raghothaman <rmukund@boogie.seas.upenn.edu>
2014-10-10 15:46:16 -0400

    Dijkstra4

--- test/mc/Dijkstra4.cpp
+++ test/mc/Dijkstra4.cpp
@@ -21,23 +21,89 @@ using namespace LTS;
 using namespace Exprs;
 using namespace MC;
 
-void DeclareMsgs(LabelledTS* TheLTS, const size_t NumProcesses)
+vector<tuple<const ExprTypeRef&, const ExprTypeRef&, const ExprTypeRef&>> DeclareMsgs(LabelledTS* const TheLTS, const size_t NumProcesses)
 {
   assert(TheLTS != nullptr);
   cout << __FILE__ << ", " << __LINE__ << ": Declaring messages." << endl;
+  vector<tuple<const ExprTypeRef&, const ExprTypeRef&, const ExprTypeRef&>> ans;
   
   for (size_t i = 0; i < NumProcesses; i++) {
-    string readDataName = string("ReadD") + lexical_cast<string>(i);
-    vector<pair<string, ExprTypeRef>> dataFields { make_pair(string("Data"), TheLTS->MakeBoolType()) };
-    auto readData = TheLTS->MakeMsgType(readDataName, dataFields);
+    string readDataLeftName = string("DataLeft") + lexical_cast<string>(i);
+    vector<pair<string, ExprTypeRef>> dataFieldsLeft { make_pair(string("Data"), TheLTS->MakeBoolType()) };
+    auto readDataLeft = TheLTS->MakeMsgType(readDataLeftName, dataFieldsLeft);
+
+    string readDataRightName = string("DataRight") + lexical_cast<string>(i);
+    vector<pair<string, ExprTypeRef>> dataFieldsRight { make_pair(string("Data"), TheLTS->MakeBoolType()) };
+    auto readDataRight = TheLTS->MakeMsgType(readDataRightName, dataFieldsRight);
     
-    string readReadyName = string("ReadR") + lexical_cast<string>(i);
+    string readReadyName = string("Ready") + lexical_cast<string>(i);
     vector<pair<string, ExprTypeRef>> readyFields { make_pair(string("Ready"), TheLTS->MakeBoolType()) };
     auto readReady = TheLTS->MakeMsgType(readReadyName, readyFields);
+    
+    ans.push_back(make_tuple(readDataLeft, readDataRight, readReady));
   }
   
   TheLTS->FreezeMsgs();
   cout << __FILE__ << ", " << __LINE__ << ": Declaring messages done." << endl;
+  return ans;
+}
+
+void BuildAutomata(LabelledTS* const TheLTS, const vector<tuple<const ExprTypeRef&, const ExprTypeRef&, const ExprTypeRef&>>& MsgTypes)
+{
+  assert(TheLTS != nullptr);
+  assert(MsgTypes.size() >= 2);
+
+  size_t NumProcesses = MsgTypes.size();
+  for (size_t i = 0; i < NumProcesses; i++) {
+    string ProcName = string("P") + boost::lexical_cast<string>(i);
+    auto TrueExp = TheLTS->MakeTrue();
+    auto BoolType = TheLTS->MakeBoolType();
+    auto Proc = TheLTS->MakeGenEFSM(ProcName, vector<ExpT>(), TrueExp, LTSFairnessType::None);
+
+    // Declare states
+    Proc->AddState("Init");
+    Proc->FreezeStates();
+
+    // Declare variables
+    Proc->AddVariable("Data", BoolType);
+    Proc->AddVariable("Ready", BoolType);
+
+    if (i + 1 < NumProcesses) {
+      Proc->AddInputMsg(get<0>(MsgTypes[i + 1])); // DataLeft of P_{i + 1}
+      Proc->AddInputMsg(get<2>(MsgTypes[i + 1])); // Ready of P_{i + 1}
+    }
+
+    if (i > 0) {
+      Proc->AddInputMsg(get<1>(MsgTypes[i - 1])); // DataRight of P_{i - 1}
+    }
+
+    Proc->AddOutputMsg(get<0>(MsgTypes[i])); // DataLeft of P_i
+    Proc->AddOutputMsg(get<1>(MsgTypes[i])); // DataRight of P_i
+    Proc->AddOutputMsg(get<2>(MsgTypes[i])); // Ready of P_i
+
+    Proc->FreezeVars();
+
+    // Declare transitions
+    Proc->AddOutputTransition("Init" /* Initial state */,
+        "Init" /* Final state */,
+        TrueExp /* Transition guard */,
+        vector<LTSAssignRef>() /* Updates */,
+        "DataLeft" /* Message name */,
+        get<0>(MsgTypes[i]) /* Message type */,
+        vector<ExpT>() /* Message params */ );
+
+    if (i == 0) {
+      // Signal source
+    } else if (i + 1 == NumProcesses) {
+      // Signal sink
+    } else {
+      // Signal transmitter
+    }
+
+    // Specify initial states
+  }
+  TheLTS->FreezeAutomata();
+  cout << __FILE__ << ", " << __LINE__ << ": Building automata done." << endl;
 }
 
 int main()
@@ -46,10 +112,9 @@ int main()
   auto TheLTS = new LabelledTS();
   
   const size_t NumProcesses = 3;
-  DeclareMsgs(TheLTS, NumProcesses);
+  auto MsgTypes = DeclareMsgs(TheLTS, NumProcesses);
+  BuildAutomata(TheLTS, MsgTypes);
   
-  TheLTS->FreezeAutomata();
-  cout << __FILE__ << ", " << __LINE__ << ": Freezing automata done." << endl;
   TheLTS->Freeze();
   cout << __FILE__ << ", " << __LINE__ << ": Freezing LTS done." << endl;
   
@@ -65,3 +130,4 @@ int main()
 
 //
 // Dijkstra4.cpp ends here
+
