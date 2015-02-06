f45b33548871e3181cf344669db0b0a3cd6954a7
Abhishek Udupa <audupa@seas.upenn.edu>
2015-02-06 17:17:44 -0500

    Changes in scripts, added MSIC2D with 3 caches

--- scripts/table-analysis/multirank.py
+++ scripts/table-analysis/multirank.py
@@ -5,23 +5,24 @@ import sys
 
 vals = []
 
-for rec in open('res24.csv', 'r'):
-    match = re.search('(.*), (\d+), (\d+)', rec)
+for rec in open('res245.csv', 'r'):
+    match = re.search('(.*), (\d+), (\d+), (\d+)', rec)
     if not match:
         continue
 
-    vals.append([match.group(1), int(match.group(2)), int(match.group(3))])
+    vals.append([match.group(1), int(match.group(2)), int(match.group(3)), int(match.group(4))])
 
 limRank = int(sys.argv[1]) if len(sys.argv) > 1 else 50
 fv2 = sorted(vals, cmp=lambda x, y: cmp(x[1], y[1]))[:limRank]
 fv4 = sorted(vals, cmp=lambda x, y: cmp(x[2], y[2]))[:limRank]
+fv5 = sorted(vals, cmp=lambda x, y: cmp(x[3], y[3]))[:limRank]
 
 fvals = []
 
 print sys.argv[0]
 for rec in fv2:
-    if rec in fv4:
-        print rec[0], rec[1], rec[2]
+    if rec in fv4 and rec in fv5:
+        print rec[0], rec[1], rec[2], rec[3]
         fvals.append(rec)
 
 # print len(fvals)
--- scripts/table-analysis/pareto.py
+++ scripts/table-analysis/pareto.py
@@ -5,12 +5,12 @@ import sys
 
 vals = []
 
-for rec in open('res24.csv', 'r'):
-    match = re.search('(.*), (\d+), (\d+)', rec)
+for rec in open('res245.csv', 'r'):
+    match = re.search('(.*), (\d+), (\d+), (\d+)', rec)
     if not match:
         continue
 
-    vals.append([match.group(1), int(match.group(2)), int(match.group(3))])
+    vals.append([match.group(1), int(match.group(2)), int(match.group(3)), int(match.group(4))])
 
 relax = float(sys.argv[1]) if len(sys.argv) > 2 else 1.0
 
@@ -19,11 +19,11 @@ fvals = []
 for rec in vals:
     betterFound = False
     for recp in vals:
-        if recp[1] < relax * rec[1] and recp[2] < relax * rec[2]:
+        if recp[1] < relax * rec[1] and recp[2] < relax * rec[2] and recp[3] < relax * rec[3]:
             betterFound = True
             break
     if not betterFound:
-        print rec[0], rec[1], rec[2]
+        print rec[0], rec[1], rec[2], rec[3]
 
 # print len(fvals)
 
